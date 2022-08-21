762b9a0e9e238035c815cd3f1435ae8d89da89a2
emrul <emrul.hoque@gmail.com>
2022-08-21 17:38:34 -0500

    added code to make http call

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// var client = new HttpClient();
+HttpClient client = new();
+// client.BaseAddress = new Uri("http://localhost:5000/");
+
+HttpResponseMessage response = await client.GetAsync("https://www.google.com");
+
+try
+{
+    if (response.IsSuccessStatusCode)
+    {
+        string result = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+    }
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("Message :{0} ", e.Message);
+
+}catch(Exception e)
+{ 
+    Console.WriteLine("Message :{0} ", e.Message);
+}finally{
+
+    Console.WriteLine("Finally - Done.");
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# http-client-app
+# HTTP Client App
+Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.
+
+---
+## Links
+- [HttpClient Class](https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=net-6.0)
